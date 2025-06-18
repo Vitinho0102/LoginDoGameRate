5e2379a5ef06e16e260681fe37b7dbf0b2f24743
Vitor Pasquarelli Cinalli <148168466+Vitinho0102@users.noreply.github.com>
2025-06-18 11:08:30 -0300

    Create User.js

---
+++ BackEnd/models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    profileImage: {
+        type: String,
+        default: null
+    },
+    collection: [{
+        type: String
+    }]
+}, {
+    timestamps: true
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User; 
